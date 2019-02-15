f0a3a896cda706e5f67228de7e2bf911c3c728a8
Simon9124 <435776313@qq.com>
2019-02-15 16:16:23 +0800

    组件setState语法糖

--- react01/src/App.js
+++ react01/src/App.js
@@ -8,6 +8,48 @@ function Welcome1(props) {
 }
 
 export default class App extends Component {
+  /* 当需要状态时 -> 1创建构造函数 2初始化状态 3更新状态 */
+
+  // 创建构造函数
+  constructor(props) {
+    super(props)
+
+    // 初始化状态
+    this.state = {
+      date: new Date(),
+      count: 0
+    }
+  }
+
+  componentDidMount = () => {
+    this.timer = setInterval(() => {
+      // 每秒更新状态
+      this.setState({
+        date: new Date()
+      })
+    }, 1000)
+
+    // *注意：setState是异步的，在回调函数内更新
+    this.setState(
+      // {
+      //   count: this.state.count + 1
+      // },
+      (prevState, prevProps) => ({
+        count: prevState.count + 1
+      }),
+      () => {
+        // *在回调函数内，setState更新
+        console.log(this.state.count)
+      }
+    )
+    // *setState未更新
+    // console.log(this.state.count)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
   formatName(user) {
     return user.firstName + ' ' + user.lastName
   }
@@ -41,6 +83,8 @@ export default class App extends Component {
         {jsx}
         {/* 组件属性传值 */}
         <Welcome1 name="CoCo" />
+        {/* 使用状态 */}
+        <p>{this.state.date.toLocaleTimeString()}</p>
       </div>
     )
   }
