4fceb6b615fe14dd7a34465ed3b6056ef06c1cc0
Simon9124 <435776313@qq.com>
2019-02-27 16:11:00 +0800

    redux存储用户登录&登出模拟接口

--- react01/src/store/user.redux.js
+++ react01/src/store/user.redux.js
@@ -7,7 +7,8 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'login':
       return { isLogin: true }
-
+    case 'logout':
+      return { isLogin: false }
     default:
       return state
   }
@@ -18,6 +19,15 @@ export function login() {
     // mock异步登录
     setTimeout(() => {
       dispatch({ type: 'login' })
+    }, 1000)
+  }
+}
+
+export function logout() {
+  return dispatch => {
+    // mock异步登出
+    setTimeout(() => {
+      dispatch({ type: 'logout' })
     }, 1000)
   }
 }
