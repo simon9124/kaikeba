aeef94f18ef6022474bd664566a5bd5660985f97
Simon9124 <435776313@qq.com>
2019-03-01 17:33:13 +0800

    generator

--- react01/src/generator/index.js
+++ react01/src/generator/index.js
@@ -25,7 +25,7 @@ function next() {
   let { value, done } = gen.next()
   // 依次打印
   console.log(value)
-  // 知道迭代完成
+  // 直到迭代完成
   if (!done) {
     next()
   }
@@ -45,15 +45,55 @@ function* say() {
 // 返回迭代器
 let it = say()
 
-// 输出{ value: '1', done: false }
-// a的值非该返回值，而是下次next参数
+// console.log(it.next())
 // it.next()仅仅执行到下一次yield，就停止了
-console.log(it.next())
+// 输出{ value: '1', done: false }
+// ****a的值非该返回值，a的值取决于下次next的传参
 
-// 输出'我是呗传进来的1'
+// console.log(it.next('我是被传进来的1'))
+// 输出'我是被传进来的1' -> 赋值给a
 // 输出{ value: '2', done: false }
-console.log(it.next('我是被传进来的1'))
 
-// 输出'我是呗传进来的2'
+// console.log(it.next('我是被传进来的2'))
+// 输出'我是被传进来的2' -> 赋值给b
 // 输出{ value: undefined, done: true }
-console.log(it.next('我是被传进来的2'))
+
+/*
+  结合Promise
+*/
+function* r(num) {
+  const r1 = yield compute(num)
+  yield compute(r1)
+}
+
+// compute为异步操作，结合Promise使用可以轻松实现异步操作队列（顺序执行2个Promise）
+function compute(num) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const ret = num * num
+      // 输出处理结果
+      console.log(ret)
+      // 操作成功
+      resolve(ret)
+    }, 1000)
+  })
+}
+
+// 不使用递归函数
+let it2 = r(2)
+//  {value:Promise,done:false}
+// it2.next().value.then(num => {
+//   it2.next(num)
+// })
+
+// 使用递归函数 -> 可处理Promise的next
+function next2(data) {
+  // 启动
+  let { value, done } = it2.next(data)
+  if (!done) {
+    value.then(num => {
+      next2(num)
+    })
+  }
+}
+next2()
