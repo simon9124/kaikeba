bc49e1ccf2699fd1918e776b909d2186504d4102
Simon9124 <435776313@qq.com>
2019-03-03 16:27:55 +0800

    引入umi

---
+++ react-umi/pages/users/index.js
@@ -0,0 +1,27 @@
+import styles from './index.css'
+import Link from 'umi/link'
+import router from 'umi/router'
+
+export default function() {
+  const users = [{ id: 'Simon', name: 'Simon' }, { id: 'CoCo', name: 'CoCo' }]
+
+  return (
+    <div className={styles.normal}>
+      <h1>Page Users List</h1>
+      <ul>
+        {/* Link 方式跳转 */}
+        {/* {users.map(user => (
+          // <li key={user.id}>{user.name}</li>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
+        ))} */}
+
+        {/* onClick 方式跳转 */}
+        {users.map(user => (
+          <li key={user.id} onClick={() => router.push(`/users/${user.id}`)}>
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
