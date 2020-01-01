e153786604bf56b84fe67288fcd49d34f54d73a9
simon9124 <435776313@qq.com>
2020-01-01 17:40:33 +0800

    webpack安装

--- webpack-learning/webpack.config.js
+++ webpack-learning/webpack.config.js
@@ -4,20 +4,18 @@ const path = require('path')
 module.exports = {
   entry: './index.js', // 入口文件
   module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            // name: '[name].[ext]' // [name]是源文件名称，[ext]是原模块后缀名
-            name: '[name]_[hash].[ext]',
-            outputPath: 'pics/', // 存放目录
-            limit: 10000 // ⼩于limit才转换成base64，否则等效与file-loader（此时url-loader依赖于file-loader）
-          }
+    rules: [{
+      test: /\.(png|jpe?g|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          // name: '[name].[ext]' // [name]是源文件名称，[ext]是原模块后缀名
+          name: '[name]_[hash].[ext]',
+          outputPath: 'pics/', // 存放目录
+          limit: 2048 // ⼩于limit才转换成base64，否则等效与file-loader（此时url-loader依赖于file-loader）
         }
       }
-    ]
+    }]
   }, // loader
   output: {
     // filename: 'bundle.js', // 打包后的文件名（默认为main.js）
