9802f3577d726193c1444d992ea0bdb2969e1361
simon9124 <435776313@qq.com>
2020-01-12 21:34:47 +0800

    hmr

--- webpack-learning/index.js
+++ webpack-learning/index.js
@@ -18,11 +18,37 @@ var pic = require('./userAvatar.jpg') // 默认不支持 -> 用loader处理
 console.log(pic)
 
 // axios默认会有跨域问题，需要配置proxy服务器代理，用"/api"代替proxy里target的值
-import axios from 'axios'
+// import axios from 'axios'
 // axios.get('http://localhost:9092/api/info').then(res => {
-axios.get('/api/info').then(res => {
-  console.log(res)
-})
+// axios.get('/api/info').then(res => {
+//   console.log(res)
+// })
+
+
+// css的hmr热模块替换（需非抽离css）
+var btn = document.createElement('button')
+btn.innerHTML = '新增'
+document.body.appendChild(btn)
+
+btn.onclick = function () {
+  var div = document.createElement('div')
+  console.log('1')
+  div.innerHTML = 'item'
+  document.body.appendChild(div)
+}
+
+// js的hmr热模块替换
+import counter from "./counter";
+import number from "./number";
+counter();
+number();
+if (module.hot) {
+  module.hot.accept("./number", function () {
+    document.body.removeChild(document.getElementById("number"));
+    number();
+  });
+}
+
 
 /* devServer */
 // webpack-dev-server - 启动服务器
@@ -39,8 +65,11 @@ axios.get('/api/info').then(res => {
 // mini-css-extract-plugin - 抽离css文件
 
 /* devServer */
+// port - 端口号
 // contentBase - 启动目录
 // open - 自动在浏览器打开
+// hot - 启动hmr，更新保存代码后，浏览器不用刷新即可自动渲染（抽离css后无效）
+// hotOnly - 即便HMR不⽣效，浏览器也不⾃动刷新
 // proxy - 服务器代理
 
 /* sourceMap */
--- webpack-learning/webpack.config.js
+++ webpack-learning/webpack.config.js
@@ -2,7 +2,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin') //自动生成html
 const { CleanWebpackPlugin } = require('clean-webpack-plugin') //自动清理dist
-const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 抽离css
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 抽离css
+const webpack = require('webpack') // 启动hmr
 
 module.exports = {
   entry: './index.js', // 入口文件
@@ -23,14 +24,14 @@ module.exports = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          // 'style-loader', // 将 JS 字符串生成为 style 节点
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../', // 公共路径
-              hmr: process.env.NODE_ENV === 'development' //开发环境配置热更新
-            }
-          }, //抽离css时，就不用style-loader了
+          'style-loader', // 将 JS 字符串生成为 style 节点
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          //   options: {
+          //     publicPath: '../', // 公共路径
+          //     hmr: process.env.NODE_ENV === 'development' //开发环境配置热更新
+          //   }
+          // }, //抽离css时，就不用style-loader了
           'css-loader', // 将 CSS 转化成 CommonJS 模块
           'postcss-loader', // 自动添加浏览器前缀
           'sass-loader' // 将 Sass 编译成 CSS
@@ -42,6 +43,8 @@ module.exports = {
     port: '3001', //端口号
     contentBase: './dist', // 启动目录
     open: true, //自动打开浏览器
+    hot: true, // 启动hmr
+    hotOnly: true, // 即便HMR不⽣效，浏览器也不⾃动刷新
     proxy: {
       '/api': {
         target: 'http://localhost:9092'
@@ -49,16 +52,17 @@ module.exports = {
     } //服务器代理
   }, // 开启服务器
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    new CleanWebpackPlugin(),
+    // new MiniCssExtractPlugin({
+    //   filename: '[name].css',
+    //   chunkFilename: '[id].css'
+    // }), // 抽离css
+    new CleanWebpackPlugin(), //自动清理dist
     new HtmlWebpackPlugin({
       title: 'html 模板',
       // filename: 'index_[hash].html',
       template: './index.html'
-    })
+    }), //自动生成html
+    new webpack.HotModuleReplacementPlugin() // 启动hm
   ], //plugins
   output: {
     // filename: 'bundle.js', // 打包后的文件名（默认为main.js）
