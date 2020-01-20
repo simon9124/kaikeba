3a7090632750ed040aa8c6207ef7c30f812e8f5b
simon9124 <435776313@qq.com>
2020-01-20 14:08:18 +0800

    手写loader

--- webpack-source/dist/main.js
+++ webpack-source/dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("console.log('loader replace async')\r\n\r\n/* 自己编写一个Loader */\r\n// https://webpack.js.org/api/loaders/\r\n// loader - 声明式函数（不能用箭头函数，因为该函数中要用到this）\r\n// this - 包含loader里面的所有内容\r\n// souce - 文件内容\r\n// this.query：options里面的参数对象\r\n// this.callback：处理options里返回多个信息\r\n// this.async：处理异步操作（返回值必须是String或Buffer，需结合callback使用）\r\n// loader-utils - 更好的管理参数，loaderUtils.getOptions(this)\r\n\n\n//# sourceURL=webpack:///./index.js?");
+eval("console.log('loader replace async')\r\n\n\n//# sourceURL=webpack:///./index.js?");
 
 /***/ })
 
--- webpack-source/webpack.config.js
+++ webpack-source/webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path')
 
 module.exports = {
+  entry: './index.js',
+  resolveLoader: {
+    // 优先到./loaders里面找，如果找不到再到node_modules里面找
+    modules: ['node_modules', './loaders/']
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: [
           {
-            loader: path.resolve(__dirname, './loaders/replaceLoaderAsync.js')
+            // loader: path.resolve(__dirname, './loaders/replaceLoaderAsync.js')
+            loader: 'replaceLoaderAsync' // 配置resolveLoader可简化路径
           },
           {
-            loader: path.resolve(__dirname, './loaders/replaceLoader.js'),
+            // loader: path.resolve(__dirname, './loaders/replaceLoader.js'),
+            loader: 'replaceLoader',
             options: {
               name: 'loader replace' // 参数对象可以通过 loader.js 的 this.query 获取到
             }
@@ -19,7 +26,6 @@ module.exports = {
       }
     ]
   },
-  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist')
   },
